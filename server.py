41eef38152f70e13b80aec1824d86ceabd5ebfde
singasanisuryateja <90127578+singasanisuryateja@users.noreply.github.com>
2022-04-17 18:16:37 +0530

    Update server.py

--- server.py
+++ server.py
@@ -24,10 +24,8 @@ def index():
 def fetch_items():
     return jsonify(items=items)
 
-'''
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
- '''
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+ 
+
